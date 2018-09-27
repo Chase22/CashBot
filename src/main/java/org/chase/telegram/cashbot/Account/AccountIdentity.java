b0bb1106b3a7a32a320880f336aad62cd1e8eb24
chase <chase@mailbox.org>
2018-09-27 22:07:50 +0200

    Add noArgs Constructor

--- src/main/java/org/chase/telegram/cashbot/Account/AccountIdentity.java
+++ src/main/java/org/chase/telegram/cashbot/Account/AccountIdentity.java
@@ -2,12 +2,14 @@ package org.chase.telegram.cashbot.Account;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Embeddable
 class AccountIdentity implements Serializable {
     private String groupId;
