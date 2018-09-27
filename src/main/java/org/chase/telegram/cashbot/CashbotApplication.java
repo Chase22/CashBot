c9dd6c604aacce705c4dd07160a5f80687d27296
chase <chase@mailbox.org>
2018-09-27 18:34:18 +0200

    Initialize new spring rewrite branch

---
+++ src/main/java/org/chase/telegram/cashbot/CashbotApplication.java
@@ -0,0 +1,12 @@
+package org.chase.telegram.cashbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashbotApplication.class, args);
+    }
+}
