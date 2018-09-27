55beecaee92b29776c6378d6a12a41893c4f3f3b
chase <chase@mailbox.org>
2018-09-27 20:43:21 +0200

    Add constructor to AccountIdentity

--- src/main/java/org/chase/telegram/cashbot/Account/AccountIdentity.java
+++ src/main/java/org/chase/telegram/cashbot/Account/AccountIdentity.java
@@ -1,11 +1,13 @@
 package org.chase.telegram.cashbot.Account;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 @Embeddable
 class AccountIdentity implements Serializable {
     private String groupId;
