9c454ec20f7a9b773fb2edf81a35c7795987811e
chase <chase@mailbox.org>
2018-09-27 20:44:24 +0200

    Add methods to change balance

--- src/main/java/org/chase/telegram/cashbot/Account/Account.java
+++ src/main/java/org/chase/telegram/cashbot/Account/Account.java
@@ -1,8 +1,10 @@
 package org.chase.telegram.cashbot.Account;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 @Data
+@Slf4j
 public class Account {
     private String groupId;
     private int userId;
@@ -13,4 +15,16 @@ public class Account {
         userId = cashChatEntity.getAccountIdentity().getUserId();
         balance = cashChatEntity.getBalance();
     }
+
+    public void addToBalance(int amount) {
+        this.setBalance(this.getBalance() + amount);
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+        log.info("Changed Balance for acount in group {} for user {} from {} to {}",
+                groupId, userId, this.balance, balance);
+    }
+
+
 }
