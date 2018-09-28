7fd9ebc1d28d5e5d4513815d7c965fea34bd06ea
chase <chase@mailbox.org>
2018-09-28 20:55:06 +0200

    Refactor registering Service

--- src/main/java/org/chase/telegram/cashbot/bot/CommandRegisterService.java
+++ src/main/java/org/chase/telegram/cashbot/bot/CommandRegisterService.java
@@ -1,5 +1,8 @@
 package org.chase.telegram.cashbot.bot;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.bridge.ICommand;
+import org.chase.telegram.cashbot.CashUser.commands.ShowMe;
 import org.chase.telegram.cashbot.commands.StartCommand;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
@@ -8,18 +11,33 @@ import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand
 import javax.inject.Inject;
 
 @Service
+@Slf4j
 public class CommandRegisterService {
     private final StartCommand startCommand;
+    private final ShowMe showMe;
 
     @Inject
-    public CommandRegisterService(StartCommand startCommand) {
+    public CommandRegisterService(final StartCommand startCommand, final ShowMe showMe) {
         this.startCommand = startCommand;
+        this.showMe = showMe;
     }
 
     void registerCommands(TelegramLongPollingCommandBot bot, IBotCommand... aditionalCommands) {
-        bot.registerAll(aditionalCommands);
-        bot.registerAll(
-                startCommand
-        );
+        log.info("Registering commands...");
+        for(IBotCommand command : aditionalCommands) {
+            register(bot, command);
+        }
+
+        register(bot, startCommand);
+        register(bot, showMe);
+        log.info("Finish registering commands");
+    }
+
+    private void register(TelegramLongPollingCommandBot bot, final IBotCommand command) {
+        if (bot.register(command)) {
+            log.info("Registered command: \"{}\"", command.getCommandIdentifier());
+        } else {
+            log.error("Error registering command: \"{}\"", command.getCommandIdentifier());
+        }
     }
 }
