81ea5c720b1fa072152642d0aa3f57f81fac68c7
chase <chase@mailbox.org>
2018-09-29 22:15:42 +0200

    Intentionally break test

--- src/main/java/org/chase/telegram/cashbot/Changelog/Version.java
+++ src/main/java/org/chase/telegram/cashbot/Changelog/Version.java
@@ -40,7 +40,7 @@ public class Version implements Comparable<Version>{
         if (o.getMajor() == major) {
             if (o.getMinor() == minor) {
                 if (o.getPatch() == patch) {
-                    return 0;
+                    return 10;
                 } else {
                     return Integer.compare(patch, o.getPatch());
                 }
