3511e0bc1d0277e55ac50df424f7f3abf4906098
chase <chase@mailbox.org>
2018-09-29 22:03:34 +0200

    Add travis CI

--- src/test/java/org/chase/telegram/cashbot/CashbotApplicationTests.java
+++ src/test/java/org/chase/telegram/cashbot/CashbotApplicationTests.java
@@ -1,13 +1,22 @@
 package org.chase.telegram.cashbot;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest({
+        "BOT_TOKEN=340581395:AAE1hZDVsts3My4yXcjA4PprGmGFLiAq9l0",
+        "PORT=8080"})
 public class CashbotApplicationTests {
+
+    @BeforeClass
+    public static void initializeApi() {
+        ApiContextInitializer.init();
+    }
 
     @Test
     public void contextLoads() {
