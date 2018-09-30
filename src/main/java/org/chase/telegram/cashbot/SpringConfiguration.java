c32625518aee359bcd24797b036d554440730c8c
chase <chase@mailbox.org>
2018-09-30 14:28:45 +0200

    Activate postgres sql

---
+++ src/main/java/org/chase/telegram/cashbot/SpringConfiguration.java
@@ -0,0 +1,31 @@
+package org.chase.telegram.cashbot;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+@Profile("prod")
+public class SpringConfiguration {
+
+    @Bean
+    public BasicDataSource dataSource(@Value("${DATABASE_URL}")final String dbUriString) throws URISyntaxException {
+        final URI dbUri = new URI(dbUriString);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
