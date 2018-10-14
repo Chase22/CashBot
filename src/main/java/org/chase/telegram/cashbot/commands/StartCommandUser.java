0d38dcd9ed4f8a3301de3cf18f005acfd5af2b6a
chase <chase@mailbox.org>
2018-10-14 10:22:09 +0200

    Fix problem with exception in lambdas

--- src/main/java/org/chase/telegram/cashbot/commands/StartCommandUser.java
+++ src/main/java/org/chase/telegram/cashbot/commands/StartCommandUser.java
@@ -28,7 +28,9 @@ public class StartCommandUser extends CashCommand {
 
     @Override
     protected void verify(final AbsSender absSender, final Message message, final String[] arguments) throws VerificationException {
-        cashUserService.getById(message.getFrom().getId()).ifPresent((cashUser) -> new VerificationException("Bot is already running"));
+        if (cashUserService.getById(message.getFrom().getId()).isPresent()) {
+            throw new VerificationException("Bot is already running");
+        }
     }
 
     @Override
