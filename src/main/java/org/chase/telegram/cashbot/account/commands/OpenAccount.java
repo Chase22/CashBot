a1be766d8d1d48a58fc57e4d4b8885a7e5b2e4fa
chase <chase@mailbox.org>
2018-10-15 21:12:46 +0200

    Make open Account check that the User is registered

--- src/main/java/org/chase/telegram/cashbot/account/commands/OpenAccount.java
+++ src/main/java/org/chase/telegram/cashbot/account/commands/OpenAccount.java
@@ -4,13 +4,13 @@ import org.chase.telegram.cashbot.VerificationException;
 import org.chase.telegram.cashbot.account.Account;
 import org.chase.telegram.cashbot.account.AccountService;
 import org.chase.telegram.cashbot.cashChat.CashChatService;
+import org.chase.telegram.cashbot.cashUser.CashUserService;
 import org.chase.telegram.cashbot.commands.CashBotReply;
 import org.chase.telegram.cashbot.commands.CashCommand;
 import org.chase.telegram.cashbot.commands.EnableCommand;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.bots.AbsSender;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.Optional;
 
@@ -25,23 +25,26 @@ public class OpenAccount extends CashCommand {
 
     private final AccountService accountService;
     private final CashChatService cashChatService;
+    private final CashUserService cashUserService;
 
-    public OpenAccount(final AccountService accountService, final CashChatService cashChatService) {
+    public OpenAccount(final AccountService accountService, final CashChatService cashChatService, final CashUserService cashUserService) {
         super(IDENTIFIER, DESCRIPTION, EXTENDED_DESCRIPTION);
         this.accountService = requireNonNull(accountService, "accountService");
         this.cashChatService = requireNonNull(cashChatService, "cashChatService");
+        this.cashUserService = cashUserService;
     }
 
     @Override
     protected void verify(final AbsSender absSender, final Message message, final String[] arguments) throws VerificationException {
         cashChatService.getById(message.getChatId()).orElseThrow(() -> new VerificationException("The bot is not started"));
+        cashUserService.getById(message.getFrom().getId()).orElseThrow(() -> new VerificationException("You are not registered with the bot"));
         if (accountService.getAccount(message.getFrom().getId(), message.getChatId()).isPresent()) {
             throw new VerificationException("User has already an account");
         }
     }
 
     @Override
-    protected Optional<CashBotReply> executeCommand(final AbsSender absSender, final Message message, final String[] arguments) throws TelegramApiException {
+    protected Optional<CashBotReply> executeCommand(final AbsSender absSender, final Message message, final String[] arguments) {
         Account account = accountService.createNew(message.getFrom().getId(), message.getChat().getId());
         return Optional.of(new CashBotReply(message.getChat().getId(), "New account created. Initial Balance: %s", account.getBalance()));
 
