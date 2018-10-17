21b82f87db918b0a23d74cc67f9755313fa24097
chase <chase@mailbox.org>
2018-10-17 21:42:03 +0200

    Add ShowConfig Command

---
+++ src/main/java/org/chase/telegram/cashbot/cashChat/commands/ShowConfig.java
@@ -0,0 +1,33 @@
+package org.chase.telegram.cashbot.cashChat.commands;
+
+import org.chase.telegram.cashbot.bot.TelegramUserRightService;
+import org.chase.telegram.cashbot.cashChat.CashChat;
+import org.chase.telegram.cashbot.cashChat.CashChatService;
+import org.chase.telegram.cashbot.commands.CashBotReply;
+import org.chase.telegram.cashbot.commands.EnableCommand;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.Optional;
+
+@EnableCommand
+public class ShowConfig extends ConfigCommand {
+    private static final String IDENTIFIER = "setCurrencyName";
+    private static final String DESCRIPTION = "Sets the Currency Name";
+    private static final String EXTENDED_DESCRIPTION = String.format("Shows the current Config for the group");
+    private final CashChatService cashChatService;
+
+    public ShowConfig(final CashChatService cashChatService, final TelegramUserRightService telegramUserRightService) {
+        super(IDENTIFIER, DESCRIPTION, EXTENDED_DESCRIPTION, cashChatService, telegramUserRightService);
+
+        this.cashChatService = cashChatService;
+    }
+
+    @Override
+    protected Optional<CashBotReply> executeCommand(final AbsSender absSender, final Message message, final String[] arguments) throws TelegramApiException {
+        CashChat cashChat = cashChatService.getById(message.getChatId()).get();
+
+        return Optional.of(new CashBotReply(message.getChatId(), cashChat.toString()));
+    }
+}
