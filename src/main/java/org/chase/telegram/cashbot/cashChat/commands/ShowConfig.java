ebc14c1eff17b59518ec0552ed8f87ca81bf8b9c
chase <chase@mailbox.org>
2018-10-25 23:09:20 +0200

    Make show config send command in private chat

--- src/main/java/org/chase/telegram/cashbot/cashChat/commands/ShowConfig.java
+++ src/main/java/org/chase/telegram/cashbot/cashChat/commands/ShowConfig.java
@@ -1,6 +1,8 @@
 package org.chase.telegram.cashbot.cashChat.commands;
 
 import org.chase.telegram.cashbot.cashChat.CashChatService;
+import org.chase.telegram.cashbot.cashUser.CashUser;
+import org.chase.telegram.cashbot.cashUser.CashUserService;
 import org.chase.telegram.cashbot.commands.CashBotReply;
 import org.chase.telegram.cashbot.commands.CashCommand;
 import org.chase.telegram.cashbot.commands.EnableCommand;
@@ -17,17 +19,28 @@ public class ShowConfig extends CashCommand {
     private static final String DESCRIPTION = "Shows the current Config";
     private static final String EXTENDED_DESCRIPTION = "Shows the current Config for the group";
     private final CashChatService cashChatService;
+    private CashUserService cashUserService;
 
-    public ShowConfig(final CashChatService cashChatService) {
+    public ShowConfig(final CashChatService cashChatService, final CashUserService cashUserService) {
         super(IDENTIFIER, DESCRIPTION, EXTENDED_DESCRIPTION);
 
         this.cashChatService = cashChatService;
+        this.cashUserService = cashUserService;
     }
 
     @Override
     public Optional<CashBotReply> executeCommand(final AbsSender absSender, final Message message, final String[] arguments) {
-        return cashChatService.getById(message.getChatId())
-                .map(cashChat -> Optional.of(new CashBotReply(message.getChatId(), cashChat.toString())))
-                .orElse(Optional.of(new CashBotReply(message.getChatId(), "The bot is not running for this chat")));
+        Optional<CashUser> cashUser = cashUserService.getById(message.getFrom().getId());
+
+        if (cashUser.isPresent()) {
+            return cashChatService.getById(message.getChatId())
+                    .map(cashChat -> {
+                        final CashBotReply cashBotReply = new CashBotReply(cashUser.get().getChatId(), cashChat.toChatString());
+                        cashBotReply.setOriginChat(message.getChatId());
+                        return Optional.of(cashBotReply);
+                    })
+                    .orElse(Optional.of(new CashBotReply(message.getChatId(), "The bot is not running for this chat")));
+        }
+        return Optional.of(new CashBotReply(message.getChatId(), "Please send /start to the bot in a private chat"));
     }
 }
